5aa04bca7c07bf3d2cc3f3a88f77ebc2f26ea9c6
Sean James <sean.james.dev@proton.me>
2023-09-07 19:40:30 +0100

    some more detailed setup/mocking on initial tests

--- cinema-tickets-javascript/src/pairtest/TicketService.js
+++ cinema-tickets-javascript/src/pairtest/TicketService.js
@@ -1,7 +1,22 @@
 import TicketTypeRequest from './lib/TicketTypeRequest.js';
 import InvalidPurchaseException from './lib/InvalidPurchaseException.js';
+import TicketPaymentService from '../thirdparty/paymentgateway/TicketPaymentService.js';
+import SeatReservationService from '../thirdparty/seatbooking/SeatReservationService.js'
 
 export default class TicketService {
+  #seatReservationService
+  #ticketPaymentService
+
+  /**
+   * 
+   * @param {SeatReservationService} seatReservationService 
+   * @param {TicketPaymentService} ticketPaymentService 
+   */
+  constructor(seatReservationService, ticketPaymentService) {
+    this.#seatReservationService = seatReservationService
+    this.#ticketPaymentService = ticketPaymentService
+  }
+
   /**
    * Should only have private methods other than the one below.
    */
--- cinema-tickets-javascript/test/pairtest/TicketService.test.js
+++ cinema-tickets-javascript/test/pairtest/TicketService.test.js
@@ -1,18 +1,51 @@
-import TicketService from '../../src/pairtest/TicketService';
-import InvalidPurchaseException from '../../src/pairtest/lib/InvalidPurchaseException';
-
 'use strict';
 
+import TicketService from '../../src/pairtest/TicketService';
+import InvalidPurchaseException from '../../src/pairtest/lib/InvalidPurchaseException';
+import SeatReservationService from '../../src/thirdparty/seatbooking/SeatReservationService';
+import TicketPaymentService from '../../src/thirdparty/paymentgateway/TicketPaymentService';
+
+// See https://jestjs.io/docs/es6-class-mocks
+jest.mock('../../src/thirdparty/seatbooking/SeatReservationService')
+jest.mock('../../src/thirdparty/paymentgateway/TicketPaymentService')
+
+beforeEach(() => {
+    SeatReservationService.mockClear()
+    TicketPaymentService.mockClear()
+})
+
 test('rejects calls with missing account ID', () => {
-    var ticketService = new TicketService();
+    var ticketService = new TicketService(
+        new SeatReservationService(),
+        new TicketPaymentService()
+    )
     expect(() => ticketService.purchaseTickets())
-        .toThrow(InvalidPurchaseException);
+        .toThrow(InvalidPurchaseException)
 })
 
 test('rejects calls with negative account ID', () => {
-    var ticketService = new TicketService();
+    var ticketService = new TicketService(
+        new SeatReservationService(),
+        new TicketPaymentService()
+    )
     expect(() => ticketService.purchaseTickets(-1, []))
         .toThrow(InvalidPurchaseException);
+})
+
+test('nothing happens if no tickets are being ordered', () => {
+    var ticketService = new TicketService(
+        new SeatReservationService(),
+        new TicketPaymentService()
+    )
+
+    const mockSeatReservationInstance = SeatReservationService.mock.instances[0]
+    const mockSeatReservation = mockSeatReservationInstance.reserveSeat
+
+    const mockPaymentInstance = TicketPaymentService.mock.instances[0]
+    const mockPayment = mockPaymentInstance.makePayment
+
+    expect (mockSeatReservation).not.toHaveBeenCalled()
+    expect (mockPayment).not.toHaveBeenCalled
 })
 
 // test('allows 1 adult ticket to be bought and seat reserved', () => {
