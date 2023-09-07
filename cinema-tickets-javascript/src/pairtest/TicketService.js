b87da6ba23546270db76c4812480574c242bee4b
Sean James <sean.james.dev@proton.me>
2023-09-07 20:45:04 +0100

    implement minimal adult ticket purchase logic

--- cinema-tickets-javascript/src/pairtest/TicketService.js
+++ cinema-tickets-javascript/src/pairtest/TicketService.js
@@ -10,7 +10,7 @@ export default class TicketService {
   /**
    * 
    * @param {SeatReservationService} seatReservationService 
-   * @param {TicketPaymentService} ticketPaymentService 
+   * @param {TicketPaymentService} ticketPaymentService
    */
   constructor(seatReservationService, ticketPaymentService) {
     this.#seatReservationService = seatReservationService
@@ -18,17 +18,57 @@ export default class TicketService {
   }
 
   /**
+   * Work out 
+   * 
+   * @param TicketTypeRequest ticketTypeRequest 
+   * @return Number
+   */
+  #requestPrice(ticketTypeRequest) {
+    const prices = {
+      "ADULT": 10,
+    }
+    const type = ticketTypeRequest.getTicketType()
+    return prices[type] * ticketTypeRequest.getNoOfTickets()
+  }
+
+  /**
+   * @param TicketTypeRequest ticketTypeRequest 
+   * @return Number
+   */
+  #requestSeats(ticketTypeRequest) {
+    const seats = {
+      "ADULT": 1,
+    }
+    const type = ticketTypeRequest.getTicketType()
+    return seats[type] * ticketTypeRequest.getNoOfTickets()
+  }
+
+  /**
    * Should only have private methods other than the one below.
    */
 
   /**
-   * @param {*} accountId 
-   * @param  {...any} ticketTypeRequests 
+   * @param * accountId 
+   * @param TicketTypeRequest[] ticketTypeRequests 
    * @throws InvalidPurchaseException
    */
   purchaseTickets(accountId, ...ticketTypeRequests) {
     if (!accountId || accountId < 0) {
       throw new InvalidPurchaseException("Missing account ID.")
+    }
+
+    // work out how many seats are needed and what to charge
+    var totalPrice = 0
+    var totalSeats = 0
+    ticketTypeRequests.forEach((item) => {
+      totalPrice += this.#requestPrice(item)
+      totalSeats += this.#requestSeats(item)
+    })
+
+    // make the payment and reserve the seats
+    if (totalSeats > 0) {
+      this.#ticketPaymentService.makePayment(accountId, totalPrice)
+      this.#seatReservationService.reserveSeat(accountId, totalSeats)
     }
   }
 }
--- cinema-tickets-javascript/test/pairtest/TicketService.test.js
+++ cinema-tickets-javascript/test/pairtest/TicketService.test.js
@@ -4,6 +4,7 @@ import TicketService from '../../src/pairtest/TicketService';
 import InvalidPurchaseException from '../../src/pairtest/lib/InvalidPurchaseException';
 import SeatReservationService from '../../src/thirdparty/seatbooking/SeatReservationService';
 import TicketPaymentService from '../../src/thirdparty/paymentgateway/TicketPaymentService';
+import TicketTypeRequest from '../../src/pairtest/lib/TicketTypeRequest';
 
 // See https://jestjs.io/docs/es6-class-mocks
 jest.mock('../../src/thirdparty/seatbooking/SeatReservationService')
@@ -28,7 +29,7 @@ test('rejects calls with negative account ID', () => {
         new SeatReservationService(),
         new TicketPaymentService()
     )
-    expect(() => ticketService.purchaseTickets(-1, []))
+    expect(() => ticketService.purchaseTickets(-1))
         .toThrow(InvalidPurchaseException);
 })
 
@@ -37,10 +38,10 @@ test('nothing happens if no tickets are being ordered', () => {
         new SeatReservationService(),
         new TicketPaymentService()
     )
+    ticketService.purchaseTickets(12345)
 
     const mockSeatReservationInstance = SeatReservationService.mock.instances[0]
     const mockSeatReservation = mockSeatReservationInstance.reserveSeat
-
     const mockPaymentInstance = TicketPaymentService.mock.instances[0]
     const mockPayment = mockPaymentInstance.makePayment
 
@@ -48,6 +49,47 @@ test('nothing happens if no tickets are being ordered', () => {
     expect (mockPayment).not.toHaveBeenCalled
 })
 
-// test('allows 1 adult ticket to be bought and seat reserved', () => {
-//     var ticketService = new TicketService()
-// })
+test('allows 1 adult ticket to be bought and seat reserved', () => {
+    var ticketService = new TicketService(
+        new SeatReservationService(),
+        new TicketPaymentService()
+    )
+    ticketService.purchaseTickets(12345, new TicketTypeRequest('ADULT', 1))
+
+    const mockSeatReservationInstance = SeatReservationService.mock.instances[0]
+    const mockSeatReservation = mockSeatReservationInstance.reserveSeat
+    const mockPaymentInstance = TicketPaymentService.mock.instances[0]
+    const mockPayment = mockPaymentInstance.makePayment
+
+    expect (mockSeatReservation).toHaveBeenCalledTimes(1)
+    expect (mockSeatReservation).toHaveBeenCalledWith(12345, 1)
+
+    expect (mockPayment).toHaveBeenCalledTimes(1)
+    expect (mockPayment).toHaveBeenCalledWith(12345, 10)
+})
+
+test('allows several adult tickets to be bought and seat reserved', () => {
+    var ticketService = new TicketService(
+        new SeatReservationService(),
+        new TicketPaymentService()
+    )
+    ticketService.purchaseTickets(12345, new TicketTypeRequest('ADULT', 3))
+
+    const mockSeatReservationInstance = SeatReservationService.mock.instances[0]
+    const mockSeatReservation = mockSeatReservationInstance.reserveSeat
+    const mockPaymentInstance = TicketPaymentService.mock.instances[0]
+    const mockPayment = mockPaymentInstance.makePayment
+
+    expect (mockSeatReservation).toHaveBeenCalledTimes(1)
+    expect (mockSeatReservation).toHaveBeenCalledWith(12345, 3)
+
+    expect (mockPayment).toHaveBeenCalledTimes(1)
+    expect (mockPayment).toHaveBeenCalledWith(12345, 30)
+})
+
+// TODO: negative ticket counts
+// TODO: non-numeric account numbers?
+// TODO: configurable ticket prices
+// TODO: multiple types ordered
+// TODO: ordering more than 20 tickets in one request
+// TODO: ordering more than 20 tickets in several requests
